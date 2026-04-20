74df3602ebf3947bf32bc30114ce9560117b9e8d
lukmanzuhdi01-cloud <lukmanzuhdi01@gmail.com>
2026-04-20 13:58:44 +0700

    Menambahkan method get

--- Base/Produk.java
+++ Base/Produk.java
@@ -2,21 +2,28 @@ package Base;
 
 public class Produk {
     private String nama;
-    String kategori;
-    protected double harga;
-    public int stok;
+    private double harga;
+    private int stok;
 
     //Construktor
     public Produk(String nama, double harga, int stok, String kategori){
         this.nama = nama;
         this.harga = harga;
         this.stok = stok;
-        this.kategori = kategori;
     }
+    public String getNama(){
+        return nama;
+    }
+    public double getHaarga(){
+        return harga;
+    }
+    public int getStock(){
+        return stok;
+    }
+
     
     public void tampilkanInfo(){
         System.out.println("Produk  :"+ nama);
-        System.out.println("Kategori:"+ kategori);
         System.out.println("Harga   :"+ harga);
         System.out.println("Stok    :"+ stok);
         System.out.println("=======================");
