a17764d1ba128cada035c1d6e91608f1974fce97
lukmanzuhdi01-cloud <lukmanzuhdi01@gmail.com>
2026-04-21 07:14:16 +0700

    Menambahkan package lain

--- Main.java
+++ Main.java
@@ -1,8 +1,13 @@
 import Base.Makanan;
-
+import Base.Elektronik;
 public class Main {
 
     public static void main(String[] args) {
+      
+      Elektronik e1 = new Elektronik("Laptop", 10000000, 2, 24);
+      e1.setGaransi(24);
+      e1.tampilData();
+
       Makanan e = new Makanan("Bakmie Goreng", 50000, 300, 30);
         e.setExpired(30);
         e.tampilData();
